0db20b70e1621de95ab9ae905aa7856c13717dc6
Koh Jhin Kiat <91869260+31-KohJhinKiat@users.noreply.github.com>
2024-12-03 15:22:47 +0800

    Updated travel app.

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,141 @@
-// Initial packing items
-const initialItems = [
-  { id: 1, description: "Shirt", quantity: 5, packed: false },
-  { id: 2, description: "Pants", quantity: 2, packed: false },
-];
+import { useState } from "react";
 
 function Logo() {
   return <h1>My Travel List</h1>;
 }
 
-function Form() {
+function Form({ addItem }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    //create new item
+    if (!description) return;
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+    };
+
+    //add input
+    addItem(newItem);
+
+    //reset input
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need to pack?</h3>
+
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+
+      <input
+        type="text"
+        placeholder="Item... "
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <button>Add</button>
     </form>
   );
 }
 
-function PackingList() {
+//Parent component template
+function PackingList({ items, onDeleteItem, onUpdateItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <li>{item.description}</li>
+        {items.map((item) => (
+          <Item 
+            item={item} 
+            key={item.id} 
+            onDeleteItem={onDeleteItem} 
+            onUpdateItem={onUpdateItem}
+            />
         ))}
       </ul>
     </div>
   );
 }
 
-function Stats() {
+//Child component template
+function Item({ item, onDeleteItem, onUpdateItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onUpdateItem(item.id)}
+      />
+
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} ({item.quantity})
+      </span>
+
+      <button onClick={() => onDeleteItem(item.id)}>Remove</button>
+    </li>
+  );
+}
+
+function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items in the list. You already packed Y (Z%).</em>
+      <em>
+        {percentage === 100
+          ? "You got everything!"
+          : `You have ${numItems} items in the list. You already packed ${numPacked} (
+          ${percentage}%).`}
+      </em>
     </footer>
   );
 }
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((prevItems) => [...prevItems, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  function handleUpdateItem(id) {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+      <Form addItem={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onUpdateItem={handleUpdateItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
